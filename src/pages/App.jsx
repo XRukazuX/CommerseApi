0e9c954ed5f5f54b817b27403fa49182de826ddd
Lucas <lucas_mamani_12@hotmail.com>
2026-02-04 16:34:46 -0300

    Portada y card de productos

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,18 +2,35 @@ import "../styles/App.css";
 import Nav from "../components/Nav";
 import { useContext } from "react";
 import { Portcontext } from "../components/Portcontext";
-
+import Local from "../assets/Local.png";
+import Local2 from "../assets/Local2.png";
 //Boostrap componente
+import Card from "react-bootstrap/Card";
 function App() {
   const { Product } = useContext(Portcontext);
   console.log("Producto", Product);
   return (
     <>
       <Nav />
+      <picture>
+        <source srcSet={Local2} media="(min-width: 600px)" />
+        <img src={Local} alt="Imagen del local" className="local" />
+      </picture>
       <div className="conteiner">
         <div className="Product">
           {Product.map((product, index) => {
-            return <img key={index} src={product.imagen}></img>;
+            return (
+              <Card style={{ width: "18rem" }} key={index}>
+                <Card.Img variant="top" src={product.imagen} />
+                <Card.Body>
+                  <Card.Title>{product.nombre}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {`${product.costo} $`}
+                  </Card.Subtitle>
+                  <Card.Text>{product.descripcion}</Card.Text>
+                </Card.Body>
+              </Card>
+            );
           })}
         </div>
       </div>
